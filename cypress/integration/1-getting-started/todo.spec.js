9211bc05ca3c2c17b88f830a6bee908b1bbce786
JeevanRadhakrishnan <49666670+Jeevan-Palo@users.noreply.github.com>
2023-05-09 00:53:00 +0800

    Update todo.spec.js

--- cypress/integration/1-getting-started/todo.spec.js
+++ cypress/integration/1-getting-started/todo.spec.js
@@ -4,21 +4,7 @@ import * as facetpage from '../support/facet';
 
 
 describe("Verify user can able to search Noiseless Headphones and apply a discount filter", () => {
-    beforeEach(() => {
-      cy.fixture("/user_input").as("admin");
-    });
-    
     it("Enter the Search keyword and click search button", function() {
-        cy.visit(this.admin.url);  
-        homepage.enterValue_Searchbar(this.admin.searchKeyword)  
+        cy.visit('https://www.google.com/'); 
     })
-
-    it('User should verify the search landing page',function(){
-        searchpage.verifySearchResult_Msg(this.admin.searchResult_text)
-    });
-
-    it('User should able to select "50 - 60" discount facet', function(){
-         facetpage.discount_filter(this.admin.discount_facet)
-    });
-
 });
